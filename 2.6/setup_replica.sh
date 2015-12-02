cb4dcf1442392ac4a6164f35fb1286d44bb99114
Renzo <renzo@Renzos-MacBook-Pro-59.local>
2015-12-02 23:27:19 +0000

    remove empty lines

--- 2.6/setup_replica.sh
+++ 2.6/setup_replica.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 RET=1
 while [[ RET -ne 0 ]]; do
     echo "=> Waiting for confirmation of MongoDB service startup"
@@ -7,8 +6,6 @@ while [[ RET -ne 0 ]]; do
     mongo admin --eval "help" >/dev/null 2>&1
     RET=$?
 done
-
 echo "=> Seting up replica"
 mongo $MONGOHOST --eval "if (rs.conf() == null){rs.initiate();}"
-
 echo "=> Done!"
