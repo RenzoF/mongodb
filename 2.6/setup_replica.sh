7d953df898505c97a3ae1a6f532dfaf66c619f58
Renzo <renzo.fraschini@gmail.com>
2015-12-02 22:08:00 +0000

    Update setup_replica.sh

--- 2.6/setup_replica.sh
+++ 2.6/setup_replica.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+RET=1
+while [[ RET -ne 0 ]]; do
+    echo "=> Waiting for confirmation of MongoDB service startup"
+    sleep 5
+    mongo admin --eval "help" >/dev/null 2>&1
+    RET=$?
+done
+
+echo "=> Seting up replica"
+mongo --eval "if (rs.conf() == null){rs.inititalize();}"
+
+echo "=> Done!"
